47803140d7f608ddcce2b5dbe23d434158db0549
Adrián Lizaga <lizagaadrian@gmail.com>
2024-05-25 17:42:23 +0200

    test: should throw UserNotLoggedInException when user is not logged in

--- kotlin/trip-service-kata/src/test/kotlin/org/craftedsw/tripservicekata/trip/TripServiceTest.kt
+++ kotlin/trip-service-kata/src/test/kotlin/org/craftedsw/tripservicekata/trip/TripServiceTest.kt
@@ -1,3 +1,17 @@
 package org.craftedsw.tripservicekata.trip
 
-class TripServiceTest
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException
+import org.craftedsw.tripservicekata.user.User
+import org.junit.Assert.assertThrows
+import kotlin.test.Test
+
+class TripServiceTest {
+    @Test
+    fun `should throw UserNotLoggedInException when user is not logged in`() {
+        val notLoggedUser = User()
+
+        assertThrows(UserNotLoggedInException::class.java) {
+            TripService().getTripsByUser(notLoggedUser)
+        }
+    }
+}
