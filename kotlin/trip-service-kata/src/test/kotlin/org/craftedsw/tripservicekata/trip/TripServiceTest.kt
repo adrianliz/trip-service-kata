c2af15c6d01abd8721c522ae87b22fed451d04d8
Adrián Lizaga <lizagaadrian@gmail.com>
2024-05-26 14:08:03 +0200

    test: should return trips of logged user when user is friend with logged user

--- kotlin/trip-service-kata/src/test/kotlin/org/craftedsw/tripservicekata/trip/TripServiceTest.kt
+++ kotlin/trip-service-kata/src/test/kotlin/org/craftedsw/tripservicekata/trip/TripServiceTest.kt
@@ -4,21 +4,30 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException
 import org.craftedsw.tripservicekata.user.User
 import org.junit.Assert.assertThrows
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class TripServiceTest {
-    class TripServiceWithLoggedUser(val user: User?) : TripService() {
+    class TripServiceWith(val user: User?, val trips: List<Trip> = emptyList()) : TripService() {
         companion object {
-            fun noLoggedUser() = TripServiceWithLoggedUser(null)
+            fun noLoggedUser() = TripServiceWith(null)
         }
 
         override fun getLoggedUser() = user
+
+        override fun findTrips(user: User) = trips
+    }
+
+    private fun friendUser(loggedUser: User): User {
+        val friendUser = User()
+        friendUser.addFriend(loggedUser)
+        return friendUser
     }
 
     @Test
     fun `should throw UserNotLoggedInException when user is not logged in`() {
         val notLoggedUser = User()
-        val tripService = TripServiceWithLoggedUser.noLoggedUser()
+        val tripService = TripServiceWith.noLoggedUser()
 
         assertThrows(UserNotLoggedInException::class.java) {
             tripService.getTripsByUser(notLoggedUser)
@@ -29,10 +38,22 @@ class TripServiceTest {
     fun `should return no trips when user is not friend with logged user`() {
         val loggedUser = User()
         val notFriendUser = User()
-        val tripService = TripServiceWithLoggedUser(loggedUser)
+        val tripService = TripServiceWith(loggedUser)
 
         val trips = tripService.getTripsByUser(notFriendUser)
 
         assertTrue { trips.isEmpty() }
+    }
+
+    @Test
+    fun `should return trips of logged user when user is friend with logged user`() {
+        val loggedUser = User()
+        val friendUser = friendUser(loggedUser)
+        val loggedUserTrips = listOf(Trip(), Trip())
+        val tripService = TripServiceWith(loggedUser, loggedUserTrips)
+
+        val trips = tripService.getTripsByUser(friendUser)
+
+        assertEquals(loggedUserTrips, trips)
     }
 }
