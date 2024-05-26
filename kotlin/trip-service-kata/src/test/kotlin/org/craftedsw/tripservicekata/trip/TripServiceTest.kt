6240b7a4381c38fea4b0fb6b94b076d135701306
Adrián Lizaga <lizagaadrian@gmail.com>
2024-05-26 13:57:31 +0200

    should return no trips when user is not friend with logged user

--- kotlin/trip-service-kata/src/test/kotlin/org/craftedsw/tripservicekata/trip/TripServiceTest.kt
+++ kotlin/trip-service-kata/src/test/kotlin/org/craftedsw/tripservicekata/trip/TripServiceTest.kt
@@ -4,6 +4,7 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException
 import org.craftedsw.tripservicekata.user.User
 import org.junit.Assert.assertThrows
 import kotlin.test.Test
+import kotlin.test.assertTrue
 
 class TripServiceTest {
     class TripServiceWithLoggedUser(val user: User?) : TripService() {
@@ -22,5 +23,16 @@ class TripServiceTest {
         assertThrows(UserNotLoggedInException::class.java) {
             tripService.getTripsByUser(notLoggedUser)
         }
+    }
+
+    @Test
+    fun `should return no trips when user is not friend with logged user`() {
+        val loggedUser = User()
+        val notFriendUser = User()
+        val tripService = TripServiceWithLoggedUser(loggedUser)
+
+        val trips = tripService.getTripsByUser(notFriendUser)
+
+        assertTrue { trips.isEmpty() }
     }
 }
